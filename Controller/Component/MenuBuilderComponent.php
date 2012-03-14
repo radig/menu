dff215b6a78123314ce04044c93cb660992930c7
Cauan Cabral <cauan@radig.com.br>
2012-03-14 14:24:50 -0400

    Corrigindo a checagem de permissão

--- Controller/Component/MenuBuilderComponent.php
+++ Controller/Component/MenuBuilderComponent.php
@@ -2,7 +2,7 @@
 /**
  * Componente que constrói um Menu baseado nas permissões
  * do usuário.
- * 
+ *
  * @package		radig.Menu.Controller.Component
  * @copyright		Radig Soluções em TI
  * @author			Radig Dev Team - suporte@radig.com.br
@@ -14,61 +14,61 @@ class MenuBuilderComponent extends Component
 {
 	/**
 	 * Referência para o controlador que está em execução
-	 * 
+	 *
 	 * @var Controller
 	 */
 	protected $Controller = null;
-	
+
 	/**
 	 * Componentes utilizados
 	 * @var array
 	 */
 	public $components = array('Auth', 'Acl', 'Session');
-	
+
 	protected  $_user = null;
-	
+
 	private $_defaultSettings = array(
 		'rootNode' => 'controllers'
 	);
-	
+
 	private $_acoTree = array();
-	
+
 	/**
 	 * Construtor padrão
-	 * 
+	 *
 	 * @param ComponentCollection $collection
 	 * @param array $settings
 	 */
 	public function __construct(ComponentCollection $collection, $settings = array())
 	{
 		parent::__construct($collection, $settings);
-		
+
 		$this->settings = Set::merge($this->_defaultSettings, $settings);
 	}
-	
+
 	/**
 	 * Inicialização do componente
-	 * 
+	 *
 	 * @param Controller $controller
 	 * @param array $settings
 	 */
 	public function initialize(&$controller)
 	{
 		$this->Controller =& $controller;
-		
+
 		$this->_user = $this->Auth->user();
 	}
-	
+
 	/**
 	 * Callback invocado imediatamente antes do Controller::beforeFilter()
-	 * 
+	 *
 	 * @param Controller $controller
 	 */
 	public function startup(&$controller)
 	{
 		$this->Controller =& $controller;
 	}
-	
+
 	/**
 	* Monta o menu para as ações permitidas
 	*
@@ -88,7 +88,7 @@ class MenuBuilderComponent extends Component
 
 		return $menu;
 	}
-	
+
 	/**
 	* Monta o menu da sidebar para as ações permitidas
 	*
@@ -107,7 +107,7 @@ class MenuBuilderComponent extends Component
 		}
 		return $sideMenu;
 	}
-	
+
 	/**
 	 * Monta o menu de configurações com ações permitidas
 	 *
@@ -124,10 +124,10 @@ class MenuBuilderComponent extends Component
 		{
 			$menu = Cache::read("User.{$this->_user['id']}.ConfigMenu");
 		}
-	
+
 		return $menu;
 	}
-	
+
 	/**
 	* Recupera árvore de Acos
 	*
@@ -139,7 +139,7 @@ class MenuBuilderComponent extends Component
 		{
 			// primeiramente verifica por cache contendo a arvore, se existir, a utiliza
 			$tree = Cache::read('User.' . $this->_user['id'] . '.Aco');
-				
+
 			if($tree !== false)
 			{
 				$this->_acoTree = $tree;
@@ -147,25 +147,25 @@ class MenuBuilderComponent extends Component
 			else
 			{
 				$this->_acoTree = $this->__buildAcoTree();
-	
+
 				// faz cache da árvore
 				Cache::write('User.' . $this->_user['id'] . '.Aco', $tree);
 			}
 		}
-	
+
 		return $this->_acoTree;
 	}
-	
+
 	/**
 	 * Constroí menu utilizando como entrada um array com elementos em cascata (recursivos)
-	 * 
+	 *
 	 * @param array $items
 	 * @param array $cacheKey
 	 */
 	protected function _build($items = array(), $cacheKey = 'Menu')
 	{
 		$menu = $this->_deepCheck($items);
-		
+
 		// varre o menu em busca de pais sem filho e sem ação (botão estético vazio)
 		foreach($menu as $key => $button)
 		{
@@ -174,23 +174,23 @@ class MenuBuilderComponent extends Component
 				unset($menu[$key]);
 			}
 		}
-		
+
 		Cache::write("User.{$this->_user['id']}.{$cacheKey}", $menu);
 
 		return $menu;
 	}
-	
+
 	/**
 	 * Verificação permissão para determinado item de menu/ação
 	 * recursivamente.
-	 * 
+	 *
 	 * @param array $items
 	 */
 	protected function _deepCheck(&$items)
 	{
 		if(!is_array($items))
 			return array();
-			
+
 		foreach($items as $k => $item)
 		{
 			if($this->_checkMenuNode($item))
@@ -208,60 +208,54 @@ class MenuBuilderComponent extends Component
 		}
 		return $items;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 */
 	protected function _checkMenuNode($menu)
 	{
-		$aco = '';
-		
+		$aco = 'controllers/';
+
 		if(!empty($menu['plugin']))
-			$aco .= Inflector::camelize($menu['plugin']);
-			
-		if(!empty($aco))
-			$aco .= '/';
-			
+			$aco .= Inflector::camelize($menu['plugin']) . '/';
+
 		if(!empty($menu['controller']))
-			$aco .= Inflector::camelize($menu['controller']);
-		
-		if(!empty($aco))
-			$aco .= '/';
-			
+			$aco .= Inflector::camelize($menu['controller']) . '/';
+
 		if(!empty($menu['action']))
 			$aco .= $menu['action'];
-		
+
 		// Caso o item de menu não tenha url, ele é autorizado por padrão
-		if(empty($aco))
+		if($aco === 'controllers/')
 			return true;
-			
+
 		$aro = '_' . $this->_user['username'];
-		
+
 		return $this->Acl->check($aro, $aco);
 	}
-	
+
 	/**
 	 * Busca e retorna a permissão para um nó na
 	 * árvore de acos
-	 * 
+	 *
 	 * @param string $alias
 	 */
 	protected function _getAcoNode($alias = '')
 	{
 		if(empty($this->_acoTree))
 			return false;
-			
+
 		$alias = Inflector::camelize($alias);
-		
+
 		foreach($this->_acoTree[$this->settings['rootNode']] as $node)
 		{
 			return $this->_deepSearch($node, $term);
 		}
 	}
-	
+
 	/**
 	 * Busca em profundidade, recursiva
-	 * 
+	 *
 	 * @param array $nodes
 	 * @param string $term
 	 */
@@ -271,22 +265,22 @@ class MenuBuilderComponent extends Component
 		{
 			return (bool)$nodes['authorized'];
 		}
-		
+
 		if(isset($nodes['childs']))
 		{
 			$authorized = true;
-			
+
 			foreach($nodes['childs'] as $child)
 			{
 				$authorized = $authorized && $this->_deepSearch($child, $term);
 			}
-			
+
 			return $authorized;
 		}
-		
+
 		return false;
 	}
-	
+
 	/**
 	 * Constroi e retorna uma árvore de ações juntamente com permissão de
 	 * acesso para o usuário logado.
@@ -296,10 +290,10 @@ class MenuBuilderComponent extends Component
 		$indexes = array();
 		$tree = array();
 		$permissions = array();
-	
+
 		// Recupera AROs relacionados ao usuário, na ordem Geral -> Específico
 		$aroTree = array_reverse($this->Acl->Aro->node(array('model' => 'User', 'foreign_key' => $this->_user['id'])));
-	
+
 		// Percorre lista de AROs relacionadas ao usuário
 		foreach($aroTree as $node)
 		{
@@ -311,42 +305,42 @@ class MenuBuilderComponent extends Component
 					'contain' => array('Aco')
 				)
 			);
-	
+
 			// Para cada ACO, computa as permissões definidas
 			foreach($aro['Aco'] as $aco)
 			{
 				$permissions[$aco['id']] = true;
 			}
 		}
-	
+
 		// inicia construção da árvore de ACOs
 		$acoTree = $this->Acl->Aco->find('all', array(
 				'contain' => array(),
 				'order' => 'Aco.lft'
 			)
 		);
-	
+
 		foreach($acoTree as $key => $acoNode)
 		{
 			$aco = $acoNode['Aco'];
-	
+
 			if(empty($aco['parent_id']))
 			{
 				$tree[$aco['id']] = $aco;
-	
+
 				$indexes[$aco['id']] = &$tree[$aco['id']];
 			}
 			// verifica se o nó pai já foi preenchido
 			else if(!empty($indexes[$aco['parent_id']]))
 			{
 				$indexes[$aco['parent_id']]['childs'][$key] = $aco;
-	
+
 				$indexes[$aco['id']] = &$indexes[$aco['parent_id']]['childs'][$key];
 			}
-	
+
 			// identifica permissão para o nó encontrado
 			$node = $aco;
-	
+
 			while(true)
 			{
 				// caso não possua permissão atrelada, mas seja um nó filho
@@ -357,13 +351,13 @@ class MenuBuilderComponent extends Component
 					{
 						// atualiza o nó corrente para o nó pai, para então verificar suas permissões
 						$node = $indexes[$node['parent_id']];
-	
+
 						continue;
 					}
 				}
 				break;
 			}
-	
+
 			if(isset($permissions[$node['id']]))
 			{
 				$permissions[$aco['id']] = $permissions[$node['id']];
@@ -372,34 +366,34 @@ class MenuBuilderComponent extends Component
 			{
 				$permissions[$aco['id']] = false;
 			}
-	
+
 			// associa permissão ao nó
 			$indexes[$aco['id']]['authorized'] = $permissions[$aco['id']];
 		}
-	
+
 		foreach($indexes as $id => $node)
 		{
 			// se autorização for true, repassa para seus 'ancestrais'
 			if($permissions[$id])
 			{
 				$aux = $node;
-	
+
 				while(true)
 				{
 					$indexes[$aux['id']]['authorized'] = true;
-	
+
 					if(!empty($aux['parent_id']))
 					{
 						$aux = $indexes[$aux['parent_id']];
-	
+
 						continue;
 					}
-	
+
 					break;
 				}
 			}
 		}
-	
+
 		return $tree;
 	}
 }
